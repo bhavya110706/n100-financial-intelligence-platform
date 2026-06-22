c070603e84f931da9e7211d15b404bbf227f653f
Bhavya Sree <bhavyasreek2006@gmail.com>
2026-06-22 11:32:50 +0530

    Sprint 1 Day 3: Implemented DQ-01 primary key validation

--- src/etl/validator.py
+++ src/etl/validator.py
@@ -1,1 +1,12 @@
-# To be implemented
+import pandas as pd
+
+
+def check_primary_key_uniqueness(df, column_name):
+    """
+    DQ-01
+    Check primary key uniqueness.
+    """
+
+    duplicates = df[df[column_name].duplicated()]
+
+    return duplicates
